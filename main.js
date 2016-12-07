7d9a4825400b761f6a9bd27d7089518e9e05f896
mikesankovich <misankovich@gmail.com>
2016-12-07 00:09:23 -0500

    initial

---
+++ main.js
@@ -0,0 +1,86 @@
+$(document).ready(() => {
+  const gameState = {
+    playing: false,
+    score: 0,
+    trialsremaining: 3,
+  }
+  const fruits = {
+    apple: {
+      path: 'images/apple.png',
+      points: 1,
+    },
+    banana: {
+      path: 'images/banana.png',
+      points: 10,
+    },
+    cherry: {
+      path: 'images/cherry.png',
+      points: 2,
+    },
+    drink: {
+      path: 'images/drink.png',
+      points: 6,
+    },
+    grape: {
+      path: 'images/grape.png',
+      points: 8,
+    },
+    lemon: {
+      path: 'images/lemon.png',
+      points: 3,
+    },
+    pineapple: {
+      path: 'images/pineapple.png',
+      points: 5,
+    },
+    skull: {
+      path: 'images/skull.png',
+      points: 'death',
+    }
+  }
+  $('#startreset').click((e) => {
+    if(gameState.playing) {
+      location.reload();
+    } else {
+      startGame()
+    }
+  })
+  const appendLives = (lives = gameState.trialsremaining) => {
+    for (let i = 0; i < lives; i++) {
+      $('#trialsremaining').append('<img class="heartimage" src="images/life.png" />');
+    }
+  }
+  const startGame = () => {
+    gameState.playing = true;
+    $('#scorevalue').html(gameState.score);
+    $('#trialsremaining').show();
+    $('#startreset').text('Reset Game');
+    appendLives();
+    startAction();
+  }
+  const startAction = () => {
+    $('#fruit1').attr('src', fruits[chooseRandom()].path)
+    $('#fruit1').show()
+    // $('#fruit1').append(`<img src=${fruits.apple} class="fruit" />`)
+  }
+  const chooseRandom = () => {
+    const randNumber = Math.floor(Math.random() * 100);
+    if (randNumber < 10) {
+      return 'skull';
+    } if (randNumber < 27) {
+      return 'apple'
+    } if (randNumber < 45) {
+      return 'cherry'
+    } if (randNumber < 61) {
+      return 'lemon'
+    } if (randNumber < 74) {
+      return 'pineapple'
+    } if (randNumber < 85) {
+      return 'drink'
+    } if (randNumber < 94) {
+      return 'grape'
+    } if (randNumber < 100) {
+      return 'banana'
+    }
+  }
+})
